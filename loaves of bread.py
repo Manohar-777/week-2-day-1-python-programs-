72694a648cfcf1890ddde922111b52bc11dd0a96
Manohar Pasupuleti <112623897+Manohar-777@users.noreply.github.com>
2022-10-09 12:57:38 +0530

    Add files via upload

---
+++ loaves of bread.py
@@ -0,0 +1,11 @@
+new_count=int(input("Enter the Number of new loaves purchased : "))
+old_count=int(input("Enter the Number of old loaves purchased : "))
+rate_old=(185-(0.6*185))*old_count
+rate_new=185*new_count
+count=new_count+old_count
+print("Total number loaves purchased = ",count)
+print("Regular Price : Rs. 185")
+print("Old loaves Amount : Rs. ",rate_old)
+print("New loaves Amount : Rs. ",rate_new)
+print("Total Amount : Rs. ",rate_old+rate_new)
+
